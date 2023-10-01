bf608b91afaed6d50dbcf2bf122a6ab1f1e314fd
ujwal <ujjwalnarkhede2003@gmail.com>
2023-10-01 11:01:58 +0530

    first commite

---
+++ program194.c
@@ -0,0 +1,52 @@
+// write a program  accept a two string from user and check whether that string  are similar or not
+// date 23 may 2023
+#include<stdio.h>
+#include<stdbool.h>
+
+bool strcmpX(char *str1, char *str2)
+{
+    while((*str1 != '\0') && (*str2 != '\0'))
+    {
+        if(*str1 != *str2)
+        {
+            break;
+        }
+        str1++;
+        str2++;
+    }
+
+    if((*str1 == '\0') && (*str2 == '\0'))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    char Arr[20];
+    char Brr[20];
+    bool bRet = false;
+
+    printf("Enter first string : \n");
+    scanf("%[^'\n']s",Arr);
+
+    printf("Enter second string : \n");
+    scanf(" %[^'\n']s",Brr);
+
+    bRet = strcmpX(Arr,Brr);    // strcmpX(100,200);
+
+    if(bRet == true)
+    {
+        printf("Both the strings are identical\n");
+    }
+    else
+    {
+        printf("Both the strings are diffrent\n");
+    }
+
+    return 0;
+}
